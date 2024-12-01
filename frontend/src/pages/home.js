81db941e4dda6763bcaadbbb817d04db2bbfea7a
Bisra Batool <bisrabatool345@gmail.com>
2024-12-01 06:33:06 -0800

    adding searchbar or background

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Navbar from '../components/navbar.js';
-import "./home.css";
+import './home.css';
+
 const Home = () => {
   return (
     <div className="App">
       <Navbar />
-      
+      <div className="home-container">
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+          <button>Search</button>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Home;
+
 
